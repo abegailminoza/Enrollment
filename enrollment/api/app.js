9dbb64ebb41872fb308dc4672ca359119248065f
abegailminoza <abbymnza3@gmail.com>
2023-01-05 18:53:50 +0800

    update

--- enrollment/api/app.js
+++ enrollment/api/app.js
@@ -1,6 +1,7 @@
 const koa = require('koa');
 const {koaBody} = require('koa-body');
 const cors = require('@koa/cors');
+const parser = require('koa-bodyparser');
 
 const app = new koa();
 
@@ -14,3 +15,14 @@ app.use(user.routes()).use(user.allowedMethods);
 app.listen(3001, function() {
     console.log("Server at http://localhost:3001/user")
 });
+
+const mongo = new koa();
+
+mongo.use(cors());
+mongo.use(parser());
+
+let cdata = require('./mongoapi.js');
+
+mongo.use(cdata.routes()).use(cdata.allowedMethods);
+
+mongo.listen(3002, () => console.log('http://localhost:3002/user'));
--- enrollment/components/User.vue
+++ enrollment/components/User.vue
@@ -1,104 +1,176 @@
 <template>
-  <div>
-    <SideBar />
+  <div class="body">
     <div class="container">
-      <br>
+      <br />
       <div class="form-inline" action="#">
-      <input type="text" id="form-name" v-model="item.name" placeholder="First Name" class="form-control">
-      <input type="text" v-model="item.lastname" placeholder="Last Name" class="form-control">
-      <button @click="addItem" class="btn btn-dark"><i class="fas fa-plus">Add</i></button>
-        </div>
-      <br><br>
-      <table class="table table-striped table-bordered table-sm">
+        <input
+          type="text"
+          id="form-name"
+          v-model="item.name"
+          placeholder="Name"
+          class="form-control"/>
+        <input
+          type="text"
+          v-model="item.course"
+          placeholder="Course"
+          class="form-control"
+          style="width: 360px"/>
+        <input
+          type="text"
+          v-model="item.year"
+          placeholder="Year"
+          class="form-control"
+          style="width: 60px"/>
+        <input
+          type="text"
+          v-model="item.section"
+          placeholder="Section"
+          class="form-control"
+          style="width: 80px"/>
+
+        <button @click="addItem" class="imgbtn">
+        </button>
+      </div>
+
+      <br /><br />
+      <table class="table table-striped table-bordered table-sm tablerows" style="border: 0px; font-size: 13px;">
         <thead class="thead-light">
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th class="col-2">Edit/Delete</th>
+          <th style="background-color: #06283d; color: #d8e4e9;">Name</th>
+          <th style="background-color: #06283d; color: #d8e4e9;">Course</th>
+          <th style="background-color: #06283d; color: #d8e4e9;">Year</th>
+          <th style="background-color: #06283d; color: #d8e4e9;">Section</th>
+          <th class="col-2" style="background-color: #06283d; color: #d8e4e9;">Edit/Del</th>
         </thead>
         <tr v-for="item in items" :key="item.name">
           <td>
-            <input v-if="item.edit" type="text" v-model="item.name">
-            <span v-else>{{item.name}} </span>
+            <input
+              v-if="item.edit"
+              type="text"
+              v-model="item.name"
+              v-on:keyup.enter="item.edit = !item.edit"/>
+            <span v-else>{{ item.name }} </span>
           </td>
           <td>
-            <input v-if="item.edit" type="text" v-model="item.lastname">
-            <span v-else>{{item.lastname}} </span>
+            <input
+              v-if="item.edit"
+              type="text"
+              v-model="item.course"
+              v-on:keyup.enter="item.edit = !item.edit"/>
+            <span v-else>{{ item.course }} </span>
           </td>
-          <td><button @click="ItemEdit(item)" class="btn btn-info"><i class="far fa-edit">edit</i></button>
-            <button @click="removeItem(item.id)" class="btn btn-danger"><i class="far fa-trash-alt">delete</i></button></td>
+          <td>
+            <input
+              v-if="item.edit"
+              type="text"
+              v-model="item.year"
+              v-on:keyup.enter="item.edit = !item.edit"/>
+            <span v-else>{{ item.year }} </span>
+          </td>
+          <td>
+            <input
+              v-if="item.edit"
+              type="text"
+              v-model="item.section"
+              v-on:keyup.enter="item.edit = !item.edit"/>
+            <span v-else>{{ item.section }} </span>
+          </td>
+          <td>
+            <button @click="ItemEdit(item)" class="imgbtn">
+            </button>
+            <button @click="removeItem(item)" class="imgbtn">
+            </button>
+          </td>
         </tr>
       </table>
+    </div>
   </div>
-  </div>
-    
-  </template>
-  
-  <script scope>
-  let url = "http://localhost:3001/user";
-    export default {
-      data() {
-      return {
-        item: {name: "", lastname: "", edit: false},
-        items: [],
-        tempData: []
-      }
-    },
-    methods:{
-      async addItem() {
-        await this.$axios.$post(url + '/create', {name: this.item.name, lastname: this.item.lastname})
-        .then((res) => {
-          console.log(res);
-          this.GetAllData();
-        })
-        .catch((err) => console.log(err));
-        this.item = [];
-      },
-      async removeItem(id){
-        await this.$axios.$post(url + '/delete', {id: id})
-        .then((res) => {
-          console.log(res);
-          this.GetAllData();
-        })
-        .catch((err) => console.log(err));
-      },
-      async GetAllData(){
-        this.items = await this.$axios.$get(url)
+</template>
+
+<script>
+const url = 'http://localhost:3002/cloud_student_data';
+export default {
+  data: function () {
+    return {
+      item: { id: 0, name: "", course: "", year: "", section: "", edit: false },
+      items: [],
+    };
+  },
+  methods: {
+    async addItem() {
+      await this.$axios.$post(url + '/insert', this.item)
       .then((res) => {
         console.log(res);
-        this.tempData = res;
-        console.log(this.items);
+        this.item = { name: "", course: "", year: "", section: "", edit: false };
+      })
+      .catch((err) => console.log(err.response.data));
+      await this.GetAllRecords();
+    },
+    async removeItem(item) {
+      await this.$axios.$post(url + '/delete', {id: item.id})
+      .then((res) => {
+        console.log("res");
       })
       .catch((err) => console.log(err));
-      this.items = this.tempData;
-      },
-      async ItemEdit(item) //For Updating
+
+      await this.GetAllRecords();
+    },
+    async GetAllRecords(){
+      await this.$axios.$get(url)
+      .then((res) => {
+        console.log(res);
+        this.items = res;
+      })
+      .catch((err) => console.log(err));
+      this.GetCurrentID();
+    },
+    async ItemEdit(item){
+      if(!item.edit)
       {
-        if(!item.edit)
-        {
-          item.edit = !item.edit
-        }
-        else
-        {
-          item.edit = !item.edit
-          await this.$axios.$post(url + '/update', {id: item.id, name: item.name, lastname: item.lastname})
-          .then((res) => {
-            console.log(res);
-            this.GetAllData();
-          })
-          .catch((err) => console.log(err));
-        }
+        item.edit = !item.edit;
+      }
+      else
+      {
+        item.edit = !item.edit;
+
+        await this.$axios.$post(url + '/update', item)
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+
+        await this.GetAllRecords();
       }
     },
-    async mounted(){
-      await this.GetAllData();
+    GetCurrentID(){
+      this.item.id = Math.max.apply(Math, this.items.map(function(o) { return o.id; })) + 1;
     }
-    }
-  </script>
-
-<style scoped>
-.form-inline input {
-  margin-right:8px;
+  },
+  async mounted() {
+    await this.GetAllRecords();
+  },
 }
+</script>
 
-
+<style>
+.body {
+  background-color: #fec261;
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: hidden;
+}
+.form-inline input {
+  margin-right: 8px;
+}
+.imgbtn {
+  border: 0px;
+  background-color: transparent;
+  width: 20px;
+  height: 20px;
+}
+.tablerows {
+  background: rgb(252, 250, 250);
+}
+.tableheader {
+  background-color: #06283d;
+  color: #d8e4e9;
+}
 </style>
